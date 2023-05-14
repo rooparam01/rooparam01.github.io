97f90e808a374a7da04048282d2246a1d5e24fd3
rooparam01 <111178057+rooparam01@users.noreply.github.com>
2023-05-14 14:05:29 +0530

    done

---
+++ index.js
@@ -0,0 +1,29 @@
+let drive = document
+  .querySelector("#Drivegoogle")
+  .addEventListener("click", openDrive);
+
+function openDrive() {
+  window.open(
+    (href =
+      "https://drive.google.com/file/d/17e1Xp0lcDd_iPcSahRMAbHVOLrE0xl7-/view?usp=sharing")
+  );
+}
+
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", mobileMenu);
+
+function mobileMenu() {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+}
+
+const navLink = document.querySelectorAll(".nav-link");
+
+navLink.forEach((n) => n.addEventListener("click", closeMenu));
+
+function closeMenu() {
+  hamburger.classList.remove("active");
+  navMenu.classList.remove("active");
+}
